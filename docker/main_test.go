b2119783f58598b2bed283ab7c889a4d72a3c351
dzove855 <dzove855@gmail.com>
2024-07-16 15:52:09 +0200

    :white_check_mark: add test by host

--- docker/main_test.go
+++ docker/main_test.go
@@ -18,3 +18,16 @@ func TestVolumeList(t *testing.T) {
         t.Fatal("No volumes found")
     }
 }
+
+func TestVolumeListByHost(t *testing.T) {
+    netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
+    netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    
+    vols, err := VolumeListByHost(1)
+    if err != nil {
+        t.Fatal(err)
+    }
+    if vols.Results[0].Host.Name != "testhost" {
+        t.Fatal(vols.Results[0].Host.Name)
+    }
+}
