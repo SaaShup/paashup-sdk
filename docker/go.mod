06c75edbd20b7425c31692f4f203b7e8c14cb3bb
dzove855 <dzove855@gmail.com>
2024-08-06 12:00:58 +0200

    :white_check_mark: add testify

--- docker/go.mod
+++ docker/go.mod
@@ -2,4 +2,10 @@ module github.com/SaaShup/paashup-sdk/docker
 
 go 1.22.2
 
-require github.com/SaaShup/paashup-sdk/netbox v0.0.0-20240716131611-f2dba91f0f38 // indirect
+require (
+	github.com/SaaShup/paashup-sdk/netbox v0.0.0-20240716131611-f2dba91f0f38 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- docker/main_test.go
+++ docker/main_test.go
@@ -3,55 +3,48 @@ package docker
 import (
     "testing"
     "github.com/SaaShup/paashup-sdk/netbox"
+    "github.com/stretchr/testify/assert"
     "os"
 )
 
 func TestHostCreate(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostCreate(HostCreateStruct{Name: "testhost", Endpoint: "http://test:test@test.com:1880/"})
-    if err != nil {
-        t.Fatal(err)
-    }
-    if host.Name != "testhost" {
-        t.Fatal(host.Name)
-    }
+    assert.Nil(err)
+    assert.Equal(host.Name, "testhost")
 }
 
 func TestRegistryCreate(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostSearchByName("testhost")
     registry, err := RegistryCreate(RegistryCreateStruct{Name: "testregistry", ServerAddress: "http://test.com:5000", Username: "test", Password: "test", Host: host.Id})
-    if err != nil {
-        t.Fatal(err)
-    }
-    if registry.Name != "testregistry" {
-        t.Fatal(registry.Name)
-    }
+    assert.Nil(err)
+    assert.Equal(registry.Name, "testregistry")
 }
 
 func TestImageCreate(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostSearchByName("testhost")
     registry, err := RegistrySearchByName("testregistry", host.Id)
 
     image, err := ImageCreate(ImageCreateStruct{Name: "testimage", Host: host.Id, Registry: registry.Id, ImageID: "0", Version: "0"})
-    if err != nil {
-        t.Fatal(err)
-    }
-    if image.Name != "testimage" {
-        t.Fatal(image.Name)
-    }
+    assert.Nil(err)
+    assert.Equal(image.Name, "testimage")
 }
 
 func TestContainerCreate(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostSearchByName("testhost")
     image, err := ImageSearchByName("testimage", host.Id)
@@ -65,117 +58,94 @@ func TestContainerCreate(t *testing.T) {
     containerS.NetworkSettings = []Network{}
 
     container, err := ContainerCreate(containerS)
-    if err != nil {
-        t.Fatal(err)
-    }
-    if container.Name != "testcontainer" {
-        t.Fatal(container.Name)
-    }
+    assert.Nil(err)
+    assert.Equal(container.Name, "testcontainer")
 }
 
 func TestVolumeCreate(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostSearchByName("testhost")
 
     volume, err := VolumeCreate(VolumeCreateStruct{Name: "testvolume", Host: host.Id, Driver: "local"})
-    if err != nil {
-        t.Fatal(err)
-    }
-    if volume.Name != "testvolume" {
-        t.Fatal(volume.Name)
-    }
+    assert.Nil(err)
+    assert.Equal(volume.Name, "testvolume")
 }
 
 func TestVolumeList(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     vols, err := VolumeList()
-    if err != nil {
-        t.Fatal(err)
-    }
-    if len(vols.Results) == 0 {
-        t.Fatal("No volumes found")
-    }
+    assert.Nil(err)
+    assert.NotZero(len(vols.Results))
 }
 
 func TestVolumeListByHost(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
-   
+    assert := assert.New(t)
+
     host, err := HostSearchByName("testhost")
     vols, err := VolumeListByHost(host.Id)
-    if err != nil {
-        t.Fatal(err)
-    }
-    if len(vols.Results) > 0 {
-        if vols.Results[0].Host.Name != "testhost" {
-            t.Fatal(vols.Results[0].Host.Name)
-        }
-    } else {
-        t.Fatal("No volumes found")
-    }
+    assert.Nil(err)
+    assert.NotZero(len(vols.Results))
+    assert.Equal(vols.Results[0].Host.Name, "testhost")
 }
 
 func TestContainerDelete(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostSearchByName("testhost")
     container, err := ContainerSearchByName(host, "testcontainer")
     err = ContainerDelete(container.Id)
-    if err != nil {
-        t.Fatal(err)
-    }
-    
+    assert.Nil(err) 
 }
 
 func TestVolumeDelete(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostSearchByName("testhost")
     volume, err := VolumeSearchByName("testvolume", host.Id)
     err = VolumeDelete(volume.Id)
-    if err != nil {
-        t.Fatal(err)
-    }
+    assert.Nil(err)
 }
 
 func TestImageDelete(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostSearchByName("testhost")
     image, err := ImageSearchByName("testimage", host.Id)
     err = ImageDelete(image.Id)
-    if err != nil {
-        t.Fatal(err)
-    }
+    assert.Nil(err)
 }
 
 func TestRegistryDelete(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
-    
+    assert := assert.New(t)
+
     host, err := HostSearchByName("testhost")
     registry, err := RegistrySearchByName("testregistry", host.Id)
     err = RegistryDelete(registry.Id)
-    if err != nil {
-        t.Fatal(err)
-    }
+    assert.Nil(err)
 }
 
 func TestHostDelete(t *testing.T) {
     netbox.NETBOX_URL = os.Getenv("NETBOX_URL")
     netbox.NETBOX_TOKEN = os.Getenv("NETBOX_TOKEN")
+    assert := assert.New(t)
 
     host, err := HostSearchByName("testhost")
     err = HostDelete(host.Id)
-    if err != nil {
-        t.Fatal(err)
-    }
-
+    assert.Nil(err)
 }
